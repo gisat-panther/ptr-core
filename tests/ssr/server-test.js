1a546ed6de49a58d5be8f07ad4ffaab7c4980234
Vojtěch Dubrovský <v.dubr@hotmail.com>
2022-05-31 12:43:46 +0200

    Update packages, remove Helmet

--- tests/ssr/server-test.js
+++ tests/ssr/server-test.js
@@ -1,10 +1,27 @@
 import {assert} from 'chai';
+import React from 'react';
 import * as ssr from '../../src/ssr/index';
 import * as server from '../../src/ssr/server';
 
 describe('ssr/server', function () {
 	describe('createRenderFn', function () {
-		it('without retries', function () {
+		it('without retries, without createDataFn defined (use default)', async function () {
+			let elCount = 0;
+			const requestCounter = ssr.createRequestCounter();
+			const renderFn = server.createRenderFn({
+				requestCounter,
+				createElFn() {
+					return <div></div>;
+				},
+			});
+
+			await renderFn().then(data => {
+				debugger;
+				assert.containsAllKeys(data, ['html']);
+			});
+		});
+
+		it('without retries', async function () {
 			let elCount = 0;
 			const requestCounter = ssr.createRequestCounter();
 			const renderFn = server.createRenderFn({
@@ -17,7 +34,7 @@ describe('ssr/server', function () {
 				},
 			});
 
-			renderFn().then(data => {
+			await renderFn().then(data => {
 				assert.deepStrictEqual(data, {data: {elCount: 1}});
 			});
 		});
